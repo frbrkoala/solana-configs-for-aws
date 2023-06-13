4e0ba20163c3b59f8ce35d2b7554b5fe4afc82aa
Nikolay Vlasov <vlason@amazon.com>
2023-06-13 14:58:00 +1000

    Added instace store volume setup scripts

--- src/scripts/user-data/node-user-data.sh
+++ src/scripts/user-data/node-user-data.sh
@@ -74,41 +74,8 @@ sudo mkdir /var/solana/data/ledger
 if [[ "$DISC_TYPE" == "none" ]]; then
   echo "Our disc type is instance store"
 
-  sudo bash -c 'sudo cat > /opt/setup-instance-store-volumes.sh <<EOF
-#!/bin/bash
-set +e
-echo "If volumes are mounted, dont do anything"
-if [ -z $(df --output=target | grep "/var/solana/data") ]; then
-  echo "Removing the last two lines from fstab if needed"
-  if [ ! -z $(grep "solana" /etc/fstab)]; then 
-    head -n -2 /etc/fstab > ./fstab.temp mv ./fstab.temp /etc/fstab
-  fi
-
-  DATA_DISC_ID=/dev/nvme1n1
-  sudo mkfs -t xfs $DATA_DISC_ID
-  sleep 10
-  DATA_DISC_UUID=$(lsblk -fn -o UUID  $DATA_DISC_ID)
-  DATA_DISC_FSTAB_CONF="UUID=$DATA_DISC_UUID /var/solana/data xfs defaults 0 2"
-  echo "DATA_DISC_ID="$DATA_DISC_ID
-  echo "DATA_DISC_UUID="$DATA_DISC_UUID
-  echo "DATA_DISC_FSTAB_CONF="$DATA_DISC_FSTAB_CONF
-  echo $DATA_DISC_FSTAB_CONF | sudo tee -a /etc/fstab
-
-  ACCOUNTS_DISC_ID=/dev/nvme2n1
-  sudo mkfs -t xfs $ACCOUNTS_DISC_ID
-  sleep 10
-  ACCOUNTS_DISC_UUID=$(lsblk -fn -o UUID $ACCOUNTS_DISC_ID)
-  ACCOUNTS_DISC_FSTAB_CONF="UUID=$ACCOUNTS_DISC_UUID /var/solana/accounts xfs defaults 0 2"
-  echo "ACCOUNTS_DISC_ID="$ACCOUNTS_DISC_ID
-  echo "ACCOUNTS_DISC_UUID="$ACCOUNTS_DISC_UUID
-  echo "ACCOUNTS_DISC_FSTAB_CONF="$ACCOUNTS_DISC_FSTAB_CONF
-  echo $ACCOUNTS_DISC_FSTAB_CONF | sudo tee -a /etc/fstab
-
-  sudo mount -a
-else
-  echo "Volumes are mounted, nothing changed"
-fi
-EOF'
+cd /opt
+sudo wget https://raw.githubusercontent.com/frbrkoala/solana-configs-for-aws/main/src/scripts/setup-instance-store-volumes.sh
 
 sudo chmod +x /opt/setup-instance-store-volumes.sh
 
